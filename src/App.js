a4129db2b286f3a7536b0627ea5752d16d7cb500
Jon20-Webdev <jonpalencia20@gmail.com>
2023-03-02 02:30:21 +0800

    add function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => setCount( count => count += 1 )} >Count is {count}</button>
+        <button onClick={() => setCount(0)} >Reset</button>
         <a
           className="App-link"
           href="https://reactjs.org"
